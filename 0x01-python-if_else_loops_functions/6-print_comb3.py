42eb0b912b0bbb90fcac97ac7d098f05df86fb74
missgevan <leviagevansekyi@gmail.com>
2022-11-30 10:36:45 -0800

    Code edit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for x in range(0, 8):
     for y in range(x + 1, 10):
-        print("{}{}".format(x, y), end=', ')
-    print("{}{}".format(x + 1, y))
+        print("{:d}{:d}".format(x, y), end=', ')
+    print("{:d}{:d}".format(x + 1, y))
