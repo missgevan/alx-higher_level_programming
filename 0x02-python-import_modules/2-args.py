f1bebdbe9369a0e6d697d62c24b7d388eeec38fc
missgevan <leviagevansekyi@gmail.com>
2022-12-07 09:47:39 -0800

    Prints number and its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    length = len(sys.argv) - 1
+    import sys
 
+    length = len(sys.argv) - 1
     if length == 0:
         print("0 arguments.")
     elif length == 1:
