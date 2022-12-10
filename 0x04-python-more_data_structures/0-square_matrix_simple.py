c9379f4dc9a2703dcd1c5d9959abee4e470a36cd
missgevan <leviagevansekyi@gmail.com>
2022-12-10 09:19:00 -0800

    Set dict

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        result_matrix = [x**2 for x in row]
+        result_matrix = [x * x for x in row]
         new_matrix.append(result_matrix)
         return new_matrix
